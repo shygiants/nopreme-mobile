4024caa8134c8a5db0231c1a2ff1e85237db5e4d
윤상훈 <shygiants@gmail.com>
2020-12-28 01:12:22 +0900

    Settings placeholders

--- nopreme-native/screens/Settings.js
+++ nopreme-native/screens/Settings.js
@@ -5,13 +5,72 @@ import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
-  RefreshControl,
+  TouchableOpacity,
 } from "react-native";
 
-export default function Settings() {
+import Stack from "../components/Stack";
+import Icon from "../components/Icon";
+
+const styles = StyleSheet.create({
+  settingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  settingText: {
+    fontSize: 18,
+  },
+  settingRightIcon: {
+    color: "#999999",
+  },
+});
+
+function Setting({ title, onPress }) {
   return (
-    <SafeAreaView>
-      <Text>SETTINGS</Text>
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingText}>{title}</Text>
+        <Icon
+          style={StyleSheet.compose(styles.settingRightIcon, { size: 24 })}
+          name="md-chevron-forward"
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default function Settings() {
+  const settings = [
+    {
+      title: "공지사항",
+      onPress: () => console.log("notice"),
+    },
+    {
+      title: "고객센터",
+      onPress: () => console.log("help"),
+    },
+    {
+      title: "정보",
+      onPress: () => console.log("info"),
+    },
+    {
+      title: "로그아웃",
+      onPress: () => console.log("signout"),
+    },
+    {
+      title: "탈퇴하기",
+      onPress: () => console.log("exit"),
+    },
+  ];
+  return (
+    <SafeAreaView style={{ backgroundColor: "white" }}>
+      <ScrollView style={{ height: "100%" }}>
+        <Stack style={{ padding: 16, gap: 16 }}>
+          {settings.map(({ title, onPress }, idx) => (
+            <Setting key={`setting-${idx}`} title={title} onPress={onPress} />
+          ))}
+        </Stack>
+      </ScrollView>
     </SafeAreaView>
   );
 }
